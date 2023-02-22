7c64e97f17cd5448087accc750d6501788bb9421
Fernando Laueles <laufernando18@gmail.com>
2023-02-22 14:40:46 -0600

    Cambio en Home Conexion a la BD y funcionalidad

--- src/components/gridcards/GridCards.js
+++ src/components/gridcards/GridCards.js
@@ -1,27 +1,122 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { useState, useEffect }  from "react";
+import { Form } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+import Button from 'react-bootstrap/Button';
+import { MdOutlineAddShoppingCart } from "react-icons/md";
+import { Panel} from 'rsuite';
 
-function GridCards(props) {
+
+function GridCards({ props, gendersData, sneakersData }) {
+
+    const [query, setQuery] = useState("")
+    const [cart, setCart] = useState([])
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 5
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+    useEffect(() => {
+        localStorage.clear();
+        localStorage.setItem('Carrito', cart);
+      });
+    const addProduct = (param) => {
+        setCart([...cart, param])
+        
+    };
 
     return (
         <div className="GridCards container mb-3">
-            <Row xs={1} lg={3} className="g-4">
-                {Array.from({ length: 3 }).map((_, idx) => (
-                    <Col key={idx}>
-                        <Card>
-                            <Card.Img variant="top" src="https://via.placeholder.com/300" />
-                            <Card.Body>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                    This is a longer card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            <br />
+            <br />
+            <Form.Group controlId="formBasicEmail">
+                <div className="input-with-icon-left">
+                    <Form.Control as="input" placeholder="Buscar Sneakers" onChange={event => setQuery(event.target.value)} style={{textAlign: 'center'}}></Form.Control>
+                </div>
+            </Form.Group>
+
+            {
+                gendersData.map((postg, index) => (
+
+                    <Panel id={index} header={postg.gender} bordered>
+
+                        <Carousel
+                            swipeable={false}
+                            draggable={false}
+                            responsive={responsive}
+                            infinite={true}
+                            autoPlaySpeed={1000}
+                            keyBoardControl={true}
+                            customTransition="all .2"
+                            transitionDuration={100}
+                            containerClass="carousel-container"
+                            removeArrowOnDeviceType={["tablet", "mobile"]}
+                            dotListClass="custom-dot-list-style">
+
+
+
+                            { 
+                                sneakersData.filter(post => {
+                                    if (query === '') {
+                                        return post;
+                                    } else if (post.marca.toLowerCase().includes(query.toLowerCase())) {
+                                        return post;
+                                    }
+                                    else if (post.marca.toUpperCase().includes(query.toUpperCase())) {
+                                        return post;
+                                    }
+                                }).map((post, index) => {
+
+                                    if (postg.gender === post.genero) {
+                                        return (
+                                            <div className='p-2' id={index} >
+                                                <Card key={post._id}>
+                                                    <Card.Img variant="top" src={post.img} style={{ height: "13rem" }} />
+                                                    <Card.Header><Card.Title style={{ height: "1rem" }} >{post.marca}</Card.Title></Card.Header>
+                                                    <Card.Body>
+                                                        <Card.Text style={{ height: "5rem" }} >
+                                                            {post.descripcion}
+                                                        </Card.Text>
+                                                        <Card.Text>
+                                                            Precio: ${post.precio}
+                                                            <br />
+                                                            Talla: {post.talla}
+                                                            <br />
+                                                            Genero: {post.genero}
+                                                        </Card.Text>
+                                                        <Button variant="primary" onClick={() => addProduct(post._id)} ><MdOutlineAddShoppingCart /></Button>
+                                                    </Card.Body>
+                                                    <Card.Footer className="text-muted">Existencia:{post.cantidad}</Card.Footer>
+                                                </Card>
+                                            </div>)
+
+                                    }
+                                })
+                            }
+
+                        </Carousel>
+                    </Panel>
+
+                ))
+
+            }
+
         </div>
     );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,73 @@
 import Header from "../../components/header/Header";
-import Slideshow from "../../components/slideshow/Slideshow";
 import GridCards from "../../components/gridcards/GridCards";
 import { isAuthenticated } from "../../utils/authHelper";
+import { Component } from "react";
 
-function Home(props) {
-  return (
-    <div className="Home">
+class Home extends Component {
+  // Constructor 
+  state = {
+    errorMessage: null,
+    successMessage: null,
+    sneakers: [],
+    genders: []
+  }
+  getSneakers = () => {
+
+    //get API url from the environment variables
+    const apiURL = process.env.REACT_APP_API_URL
+
+    //use fetch to make an API call and get a specific student (returns a promise)
+    fetch(`${apiURL}/api/sneaker`)
+        //on success of the fetch request, turn the response that came back into JSON
+        .then((response) => response.json())
+        //on success of turnig the response into JSON (data we can work with), lets add that data to state
+        .then((result) => {
+
+            //update state with the data from the API causing the page to re-render
+            this.setState({sneakers: result});
+        })
+        //handle any errors/failures with getting data from the API
+        .catch((error) => {
+            console.log(error)
+        });
+  }
+
+  getGenders = () => {
+
+    //get API url from the environment variables
+    const apiURL = process.env.REACT_APP_API_URL
+
+    //use fetch to make an API call and get a specific student (returns a promise)
+    fetch(`${apiURL}/api/gender`)
+        //on success of the fetch request, turn the response that came back into JSON
+        .then((response) => response.json())
+        //on success of turnig the response into JSON (data we can work with), lets add that data to state
+        .then((result) => {
+
+            //update state with the data from the API causing the page to re-render
+            this.setState({genders: result});
+        })
+        //handle any errors/failures with getting data from the API
+        .catch((error) => {
+            console.log(error)
+        });
+  }
+
+  componentDidMount() {
+    this.getSneakers()
+    this.getGenders()
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="Home">
         <Header isAuthenticated={isAuthenticated()} />
-        <Slideshow/>
-        <GridCards/>
-    </div>
-  );
+        <GridCards key={1} gendersData={this.state.genders} sneakersData={this.state.sneakers}/>
+      </div>
+    );
+  }
 }
-
 export default Home;
